a3b5cf261cdaa970b5d4c0b6426228e11739601c
Éttore Leandro Tognoli <ettore.leandro.tognoli@gmail.com>
2018-04-22 18:55:02 -0300

    install django & add cdi at request

--- django_pycdi/middlewares.py
+++ django_pycdi/middlewares.py
@@ -1,6 +1,9 @@
+from django.conf import settings
 from pycdi.core import DEFAULT_CONTAINER
 from pycdi.core import INJECT_ARGS
 from pycdi.core import INJECT_KWARGS
+
+CDI_NAME = getattr(settings, 'CDI_NAME', 'cdi')
 
 
 class CDIMiddleware(object):
@@ -12,11 +15,13 @@ class CDIMiddleware(object):
         return self.get_response(request)
 
     def process_view(self, request, view_func, view_args, view_kwargs):
+        sub_container = self.container.sub_container(request, **view_kwargs)
+        setattr(request, CDI_NAME, sub_container)
         inject_kwargs = getattr(view_func, INJECT_KWARGS, False)
         if not inject_kwargs:
             return None
         inject_args = getattr(view_func, INJECT_ARGS, False)
-        sub_container = self.container.sub_container(request, **view_kwargs)
+
         if len(inject_args) > 0 or 'request' in inject_kwargs:
             return sub_container.call(view_func, *view_args, **view_kwargs)
         else:
