606893c2a8d9cda9cbd08f859394c6ad30c5241d
Éttore Leandro Tognoli <ettore.leandro.tognoli@gmail.com>
2018-04-22 19:00:58 -0300

    add cdi before process_view

--- django_pycdi/middlewares.py
+++ django_pycdi/middlewares.py
@@ -12,11 +12,14 @@ class CDIMiddleware(object):
         self.container = DEFAULT_CONTAINER
 
     def __call__(self, request):
+        sub_container = self.container.sub_container(request)
+        setattr(request, CDI_NAME, sub_container)
         return self.get_response(request)
 
     def process_view(self, request, view_func, view_args, view_kwargs):
-        sub_container = self.container.sub_container(request, **view_kwargs)
-        setattr(request, CDI_NAME, sub_container)
+        sub_container = getattr(request, CDI_NAME)
+        for k, v in view_kwargs.items():
+            sub_container.register_instance(v, context=k)
         inject_kwargs = getattr(view_func, INJECT_KWARGS, False)
         if not inject_kwargs:
             return None
